04c02dfe6b5431139ecf9b1468f24af5a4a0b954
tekavec <tomaz@codurance.com>
2015-09-21 19:46:27 +0100

    AccountShould test class (missing in previous commit).

---
+++ BankKataCalisthenics.Tests/AccountShould.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace BankKataCalisthenics.Tests
+{
+    [TestClass]
+    public class AccountShould
+    {
+        private Account _account;
+        private readonly IClock _clock = Substitute.For<IClock>();
+        private readonly ITransactionRepository _transactionRepository = Substitute.For<ITransactionRepository>();
+        private static readonly DateTime DateA = new DateTime(2015, 9, 15);
+        private readonly Money _moneyA = new Money(1000m);
+        private readonly Money _moneyB = new Money(-1000m);
+
+        [TestInitialize]
+        public void Init()
+        {
+            _account = new Account(_clock, _transactionRepository);
+        }
+
+        [TestMethod]
+        public void StoreADeposit()
+        {
+            var transaction = new Transaction(_moneyA, DateA);
+            _clock.Today().Returns(DateA);
+
+            _account.Deposit(_moneyA);
+
+            _transactionRepository.Received().AddTransaction(transaction);
+        }
+
+        [TestMethod]
+        public void StoreAWithdrawal()
+        {
+            var transaction = new Transaction(_moneyB, DateA);
+            _clock.Today().Returns(DateA);
+
+            _account.Withdraw(_moneyA);
+
+            _transactionRepository.Received().AddTransaction(transaction);
+        }
+    }
+}
---
+++ BankKataCalisthenics.Tests/TransactionRepositoryShould.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankKataCalisthenics.Tests
+{
+    [TestClass]
+    public class TransactionRepositoryShould
+    {
+        private ITransactionRepository _transactionRepository;
+        private readonly Transaction _transactionA = new Transaction(new Money(1000m), new DateTime(2015, 9, 10));
+        private readonly Transaction _transactionB = new Transaction(new Money(3000m), new DateTime(2015, 8, 10));
+
+        [TestInitialize]
+        public void Init()
+        {
+            _transactionRepository = new TransactionRepository();
+        }
+
+        [TestMethod]
+        public void StoreATransaction()
+        {
+            _transactionRepository.AddTransaction(_transactionA);
+            
+            Assert.AreEqual(1, _transactionRepository.Count);
+            Assert.IsTrue(_transactionRepository.AllTransactions.Contains(_transactionA));
+            Assert.IsFalse(_transactionRepository.AllTransactions.Contains(_transactionB));
+        }
+    }
+}

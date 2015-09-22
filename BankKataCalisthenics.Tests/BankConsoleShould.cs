343d9d0da5a9f9f10171aa39c08aa10bea77c078
tekavec <tomaz@codurance.com>
2015-09-22 01:18:58 +0100

    Unit test for BankConsole.

---
+++ BankKataCalisthenics.Tests/BankConsoleShould.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using BankKataCalisthenics.Console;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankKataCalisthenics.Tests
+{
+    [TestClass]
+    public class BankConsoleShould
+    {
+
+        [TestMethod]
+        public void WriteALineToSystemConsole()
+        {
+            using (var stringWriter = new StringWriter())
+            {
+                System.Console.SetOut(stringWriter);
+
+                IBankConsole console = new BankConsole();
+                console.WriteLine("test");
+
+                string expected =
+                    string.Format("test{0}", Environment.NewLine);
+                Assert.AreEqual(expected, stringWriter.ToString());
+            }
+        }
+         
+    }
+}
